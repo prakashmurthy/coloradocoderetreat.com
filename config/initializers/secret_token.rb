0063936cc68ef0c559dc9dd8e6f57199b85ffc38
Mr. Matt <system9@composition9.com>
2011-11-23 16:03:04 -0700

    rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coloradocoderetreat::Application.config.secret_token = '90c7e9b773902c80deb1e4e504adb5f6c598707d46b95e489c1316c3aa66069f25d4c09672b4757f549b4b5e0a343f6ae6ccaa19343b8412bb7b6694ad844348'
